4c1816bdebc7dd18ddc83b8ea4d16e1b126fae30
fallon-r <fallon_ryan@protonmail.com>
2024-09-07 21:18:18 -0400

    App pre modifications

--- README.md
+++ README.md
@@ -5,3 +5,8 @@ Followed [this article](https://dev.to/ethand91/building-a-todo-crud-app-with-re
 
 
 Getting my feet wet.
+
+----
+
+<!-- 2024-09-07 -->
+This is done based on the article, but I am going to begin breaking things soon 
--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,116 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, {
+  useState,
+  useEffect
+} from 'react';
+import axios from 'axios';
+import {
+  Box,
+  Button,
+  Input,
+  VStack,
+  Checkbox,
+  HStack,
+  Container,
+  Heading,
+  Center
+} from '@chakra-ui/react';
 
-function App() {
-  const [count, setCount] = useState(0)
+type Todo = {
+  id: number;
+  title: string;
+  completed: boolean;
+};
+
+const App = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [newTodo, setNewTodo] = useState<string>('');
+
+  useEffect(() => {
+    const fetchTodos = async (): Promise<void> => {
+      const response = await axios.get<Todo[]>('http://localhost:5000/todos');
+      setTodos(response.data);
+    };
+
+    fetchTodos();
+  }, []);
+
+  const addTodo = async (): Promise<void> => {
+    const response = await axios.post<Todo>('http://localhost:5000/todos', {
+      title: newTodo,
+      completed: false
+    });
+
+    setTodos([...todos, response.data]);
+    setNewTodo('');
+  };
+
+  const updateTodo = async (id: number, title: string): Promise<void> => {
+    const updatedTodo = { title, completed: false };
+    await axios.put(`http://localhost:5000/todos/${id}`);
+    setTodos(todos.map(todo => (todo.id === id ? { ...todo, title } : todo)));
+  };
+
+  const deleteTodo = async (id: number): Promise<void> => {
+    await axios.delete(`http://localhost:5000/todos/${id}`);
+    setTodos(todos.filter(todo => todo.id !== id));
+  };
+
+  const toggleComplete = async (id: number): Promise<void> => {
+    const todo = todos.find(todo => todo.id === id);
+
+    if (todo) {
+      const updatedTodo = { ...todo, completed: !todo.completed };
+      await axios.put(`http://localhost:5000/todos/${id}`, updatedTodo);
+      setTodos(todos.map(todo => (todo.id === id ? updatedTodo : todo)));
+    }
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <Container maxW="container.md" p={5}>
+      <Center>
+        <Heading mb={5}>Simple Todo App</Heading>
+      </Center>
 
-export default App
+      <Input
+        value={ newTodo }
+        onChange={
+          (e) => setNewTodo(e.target.value)
+        }
+        placeholder="Add a new todo"
+      />
+
+      <Center mt={2} mb={5}>
+        <Button
+          onClick={ addTodo }
+          mt={2}
+        >Add Todo</Button>
+      </Center>
+
+      <VStack spacing={ 4 }>
+        { todos.map(todo => (
+          <HStack key={ todo.id }>
+            <Checkbox
+              isChecked={ todo.completed }
+              onChange={
+                () => toggleComplete(todo.id)
+              }
+            />
+            <Input
+              defaultValue={ todo.title }
+              onBlur={
+                (e) => updateTodo(todo.id, e.target.value)
+              }
+            />
+            <Button onClick={
+              () => deleteTodo(todo.id)
+            }>
+              Delete
+            </Button>
+          </HStack>
+        ))}
+      </VStack>
+    </Container>
+  );
+};
+
+export default App;
